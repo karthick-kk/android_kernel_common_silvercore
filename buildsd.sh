9a76b69b84cc929d0d7429048d956b91f72c367b
karthick-kk <kkzone@gmail.com>
2024-05-04 11:40:34 +0530

    Merge tag 5.10.213

--- buildsd.sh
+++ buildsd.sh
@@ -1,8 +1,8 @@
-export PATH=/home/kk/dev/android/proprietary_vendor_qcom_sdclang/compiler/bin/:${PATH}
+export PATH=/zfsdata/build/kernel/17.0.2.0/bin/:${PATH}
 cp Makefile Makefile.orig
 cp Makefile.sd Makefile
 #ccache clang -print-supported-cpus
 make ARCH=arm64 LLVM=1 LLVM_IAS=1 O=out mrproper
-make SHELL="sh -e" ARCH=arm64 LLVM=1 LLVM_IAS=1 HOSTCC="ccache clang -target x86_64-unknown-linux-gnu"  O=out  -j 12 gki_defconfig
-make SHELL="sh -e" ARCH=arm64 LLVM=1 LLVM_IAS=1 HOSTCC="ccache clang -target x86_64-unknown-linux-gnu"  O=out -j 12 2>&1 | tee build.log
+make SHELL="sh -e" ARCH=arm64 LLVM=1 LLVM_IAS=1 HOSTCC="ccache clang -target x86_64-unknown-linux-gnu"  O=out  -j 32 gki_defconfig
+make SHELL="sh -e" ARCH=arm64 LLVM=1 LLVM_IAS=1 HOSTCC="ccache clang -target x86_64-unknown-linux-gnu"  O=out -j 32 2>&1 | tee build.log
 mv Makefile.orig Makefile
